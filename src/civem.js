b365095d69397f54b5a680a83f2c4c56a653d3f5
Hannu Leinonen <hleinone@gmail.com>
2012-02-29 01:42:37 +0200

    Wrapper inside a function and added textarea and select element support.

--- src/civem.js
+++ src/civem.js
@@ -6,39 +6,50 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  */
-var inputs = document.getElementsByTagName("input");
-for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-    if (!input.willValidate)
-        continue;
-    if (input.oninput)
-        var inputCallback = input.oninput;
-    input.oninput = function(event) {
-        event.target.setCustomValidity("");
-        if (inputCallback)
-            inputCallback(event);
-    };
-    if (input.onchange)
-        var changeCallback = input.onchange;
-    input.onchange = function(event) {
-        event.target.checkValidity();
-        if (changeCallback)
-            changeCallback(event);
-    }
-    if (input.oninvalid)
-        var invalidCallback = input.oninvalid;
-    input.oninvalid = function(event) {
-        var element = event.target;
-        var validity = element.validity;
-        var postfix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
-        var specificErrormessage, genericErrormessage;
-        if (postfix && (specificErrormessage = element.getAttribute("data-errormessage-" + postfix)))
-            element.setCustomValidity(specificErrormessage);
-        else if (genericErrormessage = element.getAttribute("data-errormessage"))
-            element.setCustomValidity(genericErrormessage);
-        else
-            element.setCustomValidity(element.validationMessage);
-        if (invalidCallback)
-            invalidCallback(event);
-    }
-}
+(function(d) {
+	var formElements = [];
+	var inputElements = d.getElementsByTagName("input");
+	for (var i = 0; i < inputElements.length; i++)
+		formElements.push(inputElements[i]);
+	var textareaElements = d.getElementsByTagName("textarea");
+	for (var i = 0; i < textareaElements.length; i++)
+		formElements.push(textareaElements[i]);
+	var selectElements = d.getElementsByTagName("select");
+	for (var i = 0; i < selectElements.length; i++)
+		formElements.push(selectElements[i]);
+	for (var i = 0; i < formElements.length; i++) {
+		var formElement = formElements[i];
+		if (!formElement.willValidate)
+			continue;
+		if (formElement.oninput)
+			var inputCallback = formElement.oninput;
+		formElement.oninput = function(event) {
+			event.target.setCustomValidity("");
+			if (inputCallback)
+				inputCallback(event);
+		};
+		if (formElement.onchange)
+			var changeCallback = formElement.onchange;
+		formElement.onchange = function(event) {
+			event.target.checkValidity();
+			if (changeCallback)
+				changeCallback(event);
+		}
+		if (formElement.oninvalid)
+			var invalidCallback = formElement.oninvalid;
+		formElement.oninvalid = function(event) {
+			var element = event.target;
+			var validity = element.validity;
+			var postfix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
+			var specificErrormessage, genericErrormessage;
+			if (postfix && (specificErrormessage = element.getAttribute("data-errormessage-" + postfix)))
+				element.setCustomValidity(specificErrormessage);
+			else if (genericErrormessage = element.getAttribute("data-errormessage"))
+				element.setCustomValidity(genericErrormessage);
+			else
+				element.setCustomValidity(element.validationMessage);
+			if (invalidCallback)
+				invalidCallback(event);
+		}
+	}
+}(document));
