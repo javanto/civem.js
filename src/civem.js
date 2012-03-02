41212d99ad0276f183ec45560704405d6ca46fdd
Hannu Leinonen <hleinone@gmail.com>
2012-03-03 00:18:51 +0200

    Removed onchange listener since browsers don't do it like that.

--- src/civem.js
+++ src/civem.js
@@ -29,21 +29,14 @@
 				if (inputCallback)
 					inputCallback(event);
 			};
-			if (formElement.onchange)
-				var changeCallback = formElement.onchange;
-			formElement.onchange = function(event) {
-				event.target.checkValidity();
-				if (changeCallback)
-					changeCallback(event);
-			}
 			if (formElement.oninvalid)
 				var invalidCallback = formElement.oninvalid;
 			formElement.oninvalid = function(event) {
 				var element = event.target;
 				var validity = element.validity;
-				var postfix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
+				var suffix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
 				var specificErrormessage, genericErrormessage;
-				if (postfix && (specificErrormessage = element.getAttribute("data-errormessage-" + postfix)))
+				if (suffix && (specificErrormessage = element.getAttribute("data-errormessage-" + suffix)))
 					element.setCustomValidity(specificErrormessage);
 				else if (genericErrormessage = element.getAttribute("data-errormessage"))
 					element.setCustomValidity(genericErrormessage);
