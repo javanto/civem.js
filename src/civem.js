0cb4db2c1a3af2764f23bf77e3ee649afb57b3f1
Hannu Leinonen <hleinone@gmail.com>
2012-12-17 14:21:52 +0200

    Lint likes this more now.

--- src/civem.js
+++ src/civem.js
@@ -1,52 +1,68 @@
-/**
- * {{= it.title}} v{{= it.version}}
- *
- * Copyright 2012, {{= it.author.name}} <{{= it.author.email}}>
- * Dual licensed under the MIT and GPL licenses:
- *   http://www.opensource.org/licenses/mit-license.php
- *   http://www.gnu.org/licenses/gpl.html
- */
 (function() {
+	function getInputHandler(inputCallback) {
+		return function(event) {
+			event.target.setCustomValidity("");
+			if (inputCallback) {
+				inputCallback(event);
+			}
+		};
+	}
+
+	function getInvalidHandler(invalidCallback) {
+		return function(event) {
+			var element = event.target;
+			var validity = element.validity;
+			var suffix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
+			var specificErrormessage, genericErrormessage;
+			if (suffix && (specificErrormessage === element.getAttribute("data-errormessage-" + suffix))) {
+				element.setCustomValidity(specificErrormessage);
+			} else if (genericErrormessage === element.getAttribute("data-errormessage")) {
+				element.setCustomValidity(genericErrormessage);
+			} else {
+				element.setCustomValidity(element.validationMessage);
+			}
+			if (invalidCallback) {
+				invalidCallback(event);
+			}
+		};
+	}
+
 	var civem = function() {
 		var formElements = [];
 		var inputElements = document.getElementsByTagName("input");
-		for (var i = 0; i < inputElements.length; i++)
+		var i;
+		for (i = 0; i < inputElements.length; i++) {
 			formElements.push(inputElements[i]);
+		}
 		var textareaElements = document.getElementsByTagName("textarea");
-		for (var i = 0; i < textareaElements.length; i++)
+		for (i = 0; i < textareaElements.length; i++) {
 			formElements.push(textareaElements[i]);
+		}
 		var selectElements = document.getElementsByTagName("select");
-		for (var i = 0; i < selectElements.length; i++)
+		for (i = 0; i < selectElements.length; i++) {
 			formElements.push(selectElements[i]);
-		for (var i = 0; i < formElements.length; i++) {
+		}
+		for (i = 0; i < formElements.length; i++) {
 			var formElement = formElements[i];
-			if (!formElement.willValidate)
+			if (!formElement.willValidate) {
 				continue;
-			if (formElement.oninput)
-				var inputCallback = formElement.oninput;
-			formElement.oninput = function(event) {
-				event.target.setCustomValidity("");
-				if (inputCallback)
-					inputCallback(event);
-			};
-			if (formElement.oninvalid)
-				var invalidCallback = formElement.oninvalid;
-			formElement.oninvalid = function(event) {
-				var element = event.target;
-				var validity = element.validity;
-				var suffix = validity.valueMissing? "value-missing" : validity.typeMismatch? "type-mismatch" : validity.patternMismatch? "pattern-mismatch" : validity.tooLong? "too-long" : validity.rangeUnderflow ? "range-underflow" : validity.rangeOverflow? "range-overflow" : validity.stepMismatch? "step-mismatch" : validity.customError? "custom-error" : "";
-				var specificErrormessage, genericErrormessage;
-				if (suffix && (specificErrormessage = element.getAttribute("data-errormessage-" + suffix)))
-					element.setCustomValidity(specificErrormessage);
-				else if (genericErrormessage = element.getAttribute("data-errormessage"))
-					element.setCustomValidity(genericErrormessage);
-				else
-					element.setCustomValidity(element.validationMessage);
-				if (invalidCallback)
-					invalidCallback(event);
 			}
+			var inputCallback;
+			if (formElement.oninput) {
+				inputCallback = formElement.oninput;
+			}
+			formElement.oninput = getInputHandler(inputCallback);
+			var invalidCallback;
+			if (formElement.oninvalid) {
+				invalidCallback = formElement.oninvalid;
+			}
+			formElement.oninvalid = getInvalidHandler(invalidCallback);
 		}
 	};
 
-	window.addEventListener ? window.addEventListener('load', civem, false) : (window.attachEvent? window.attachEvent('onload', civem) : null);
+	if (window.addEventListener) {
+		window.addEventListener('load', civem, false);
+	} else if (window.attachEvent) {
+		window.attachEvent('onload', civem);
+	}
 }());
